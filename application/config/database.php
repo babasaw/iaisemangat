618297968c3e9995730a1032d95c81c6d7370631
Baskoro Adi Wicaksono <baskorosatu@gmail.com>
2019-05-27 17:32:45 +0700

    third commit

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'iai',
+	'hostname' => 'sql7.freemysqlhosting.net',
+	'username' => 'sql7293481',
+	'password' => 'lHVM85yVGR',
+	'database' => 'sql7293481',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
