bfe7cf9d3356d4ce09fba8bfd6cbfeb58800d76d
Baskoro Adi Wicaksono <baskorosatu@gmail.com>
2019-05-28 15:31:19 +0700

    vue frontend

--- application/views/frontend.php
+++ application/views/frontend.php
@@ -20,6 +20,6 @@
 	</div>
 
 	<!-- <script src= "<?php echo base_url()?>assets/js/app.js"></script> -->
-	<script src= "/iaisemangat/assets/js/app.js"></script>
+	<script src= "<?php echo base_url()?>assets/js/app.js"></script>
 </body>
 </html>
